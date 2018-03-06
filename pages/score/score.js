252da85100715a3de16b3e0975a329a65d9f7050
Airmole <745362916@qq.com>
2018-03-06 19:32:13 +0800

    该死！API返回的json格式不给力
    现存挡一下吧，json格式不给力，微信小程序遍历起来不爽，只能再改改该死的后端API接口处理程序了。。。。。。好气啊

--- pages/score/score.js
+++ pages/score/score.js
@@ -1,4 +1,8 @@
 Page({
+  data: {
+    stuId: '',
+    password: '',
+  },
   submitInfo: function (e) {
     if (e.detail.value.stuId.length == 0 || e.detail.value.password.length == 0) {
       wx.showToast({
@@ -8,24 +12,9 @@ Page({
         duration: 2000
       });
     } else {
-      // console.log(e.detail.value.stuId);
-      // console.log(e.detail.value.password);
-      wx.request({
-        url: 'https://airmole.cn/test/record.php',
-        method: "POST",
-        data: {
-          stuId: e.detail.value.stuId,
-          password: e.detail.value.password
-        },
-        header: {
-          'content-type': 'application/x-www-form-urlencoded' // post提交表单
-        },
-        success: function (res) {
-          console.log("提交成功！");
-          wx.navigateTo({
-            url: '/pages/score/showScore/showScore?stuId=' + e.detail.value.stuId + '&password=' + e.detail.value.password
-          })
-        }
+      //将学号和教务密码发送到showScore页面
+      wx.navigateTo({
+        url: '/pages/score/showScore/showScore?stuId=' + e.detail.value.stuId + '&password=' + e.detail.value.password
       })
     }
   },
--- pages/score/showScore/showScore.js
+++ pages/score/showScore/showScore.js
@@ -1,66 +1,87 @@
 // pages/score/showScore/showScore.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-  
+    stuId: " ",
+    password: " ",
+    jsonContent: {},
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let that = this;
+    that.setData({
+      stuId: options.stuId,
+      password: options.password,
+    });
+
+    wx.request({
+      url: 'https://airmole.cn/test/record.php',
+      method: "POST",
+      data: {
+        stuId: options.stuId,
+        password: options.password
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // post提交表单
+      },
+      success: function (res) {
+        that.setData({
+          jsonContent: res.data
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
