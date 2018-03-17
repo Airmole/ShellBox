c4e96902e6162369452ea4a1d3a3fc1859e38cf6
Airmole <745362916@qq.com>
2018-03-17 20:54:04 +0800

    通讯录号码分离为两块
    将所有的电话号码分离为部门电话和老师电话两部分。下一步：查看老师电话需要输入自己的学号才能查看。

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const openIdUrl = require('./config').openIdUrl
 
 App({
   onLaunch: function () {
-    console.log('App Launch')
+    // console.log('App Launch')
   },
   onShow: function () {
-    console.log('App Show')
+    // console.log('App Show')
   },
   onHide: function () {
     console.log('App Hide')
---
+++ pages/tel/teacherTel/teacherTel.js
@@ -0,0 +1,74 @@
+// pages/tel/teacherTel/teacherTel.js
+var base64 = require("../../../images/base64");
+Page({
+  data: {
+    //存储老师们的联系电话的数组
+    telNumber: [{
+      name: '吴思',
+      tel: 15922164710
+    }, {
+      name: '杨灿',
+      tel: 18630801799
+    }, {
+      name: '刘卫洁',
+      tel: 18722136008
+    }],
+    //存储老师们的QQ号码的数组
+    QQNumber: [{
+      name: '吴思',
+      qq: 925396533
+    }, {
+      name: '鲍勇',
+      qq: 778547694
+    }, {
+      name: '张艳君',
+      qq: 911615726
+    }, {
+      name: '刘卫洁',
+      qq: 315226353
+    }]
+  },
+  onLoad: function () {
+    this.setData({
+      icon: base64.icon20
+    });
+  },
+  callPhone: function (event) {
+    wx.makePhoneCall({
+      phoneNumber: event.target.id
+    })
+  },
+  copyIt: function (event) {
+    wx.setClipboardData({
+      data: event.target.id
+    })
+    wx.showToast({
+      title: '已复制到粘贴版',
+      icon: 'none',
+      duration: 1000
+    });
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    //模拟加载
+    setTimeout(function () {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1500);
+  },
+  /**
+ * 用户点击右上角分享
+ */
+  onShareAppMessage: function (res) {
+    return {
+      title: '贝壳田园信息系老师联系方式',
+      path: 'pages/tel/teacherTel/teacherTel',
+      imageUrl: "https://airmole.cn/wechat/wxapp/images/QueryTel.jpg"
+    }
+  }
+})
