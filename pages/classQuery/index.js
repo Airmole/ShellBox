4d9b2ef970a531fdb463285265df4505b994ccf5
Airmole <745362916@qq.com>
2018-04-09 14:21:25 +0800

    修复课表分享出去后，好友打开无法获取到全局变量的bug

--- pages/classQuery/index.js
+++ pages/classQuery/index.js
@@ -11,27 +11,34 @@ Page({
       uid: app.globalData.uid,
       pwd: app.globalData.pwd,
     });
-    wx.request({
-      url: 'https://airmole.cn/wechat/wxapp/api/Airmole_jiaowuClassTable.php?uid=' + app.globalData.uid + '&pwd=' + app.globalData.pwd,
-      success: function (res) {
-        that.setData({
-          classStr: res.data,
-        })
-        // console.log(res.data);
-      }
-    })
+    if (app.globalData.uid == '' || app.globalData.pwd == '') {
+      wx.redirectTo({
+        url: '/pages/index/index'
+      })
+    } else {
+      wx.request({
+        url: 'https://airmole.cn/wechat/wxapp/api/Airmole_jiaowuClassTable.php?uid=' + app.globalData.uid + '&pwd=' + app.globalData.pwd,
+        success: function (res) {
+          that.setData({
+            classStr: res.data,
+          })
+          // console.log(res.data);
+        }
+      })
+    }
+
   },
   /**
  * 用户点击右上角分享
  */
-  onShareAppMessage: function (res) {
+  // onShareAppMessage: function (res) {
 
-    return {
-      title: '贝壳盒子课表查询',
-      path: 'pages/index/index',
-      imageUrl: "https://airmole.cn/wechat/wxapp/images/QueryClassTable.jpg"
-    }
-  },
+  //   return {
+  //     title: '贝壳盒子课表查询',
+  //     path: 'pages/index/index',
+  //     imageUrl: "https://airmole.cn/wechat/wxapp/images/QueryClassTable.jpg"
+  //   }
+  // },
   onPullDownRefresh: function () {
 
   },
--- pages/stuInfo/stuInfo.js
+++ pages/stuInfo/stuInfo.js
@@ -35,20 +35,27 @@ Page({
       uid: app.globalData.uid,
       pwd: app.globalData.pwd,
     });
-    wx.request({
-      url: 'https://airmole.cn/wechat/wxapp/api/Airmole_jiaowuInfoQuery.php?uid=' + app.globalData.uid + '&pwd=' + app.globalData.pwd,
-      success: function (res) {
-        that.setData({
-          jsonStr: res.data,
-        })
-        // console.log(res.data);
-        //账号密码错误以下功能实现跳转错误页面
-        if (res.data[0][0].stuName == '') {
-          wx.redirectTo({
-            url: '/pages/error/queryerror'
+    //如果是打开的是分享过来的页面，全局变量uid&pwd是undefined状态，重定向到登录页面
+    if (app.globalData.uid == '' || app.globalData.pwd == '') {
+      wx.redirectTo({
+        url: '/pages/index/index'
+      })
+    } else {
+      wx.request({
+        url: 'https://airmole.cn/wechat/wxapp/api/Airmole_jiaowuInfoQuery.php?uid=' + app.globalData.uid + '&pwd=' + app.globalData.pwd,
+        success: function (res) {
+          that.setData({
+            jsonStr: res.data,
           })
+          // console.log(res.data);
+          //账号密码错误以下功能实现跳转错误页面
+          if (res.data[0][0].stuName == '') {
+            wx.redirectTo({
+              url: '/pages/error/queryerror'
+            })
+          }
         }
-      }
-    })
+      })
+    }
   }
 });
